4d97c45eab8386d591c8553bbdc37107ef9094bd
mhdyaseen8841 <mhdyaseen.official@gmail.com>
2025-07-19 17:02:08 +0530

    changes for base url

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // export const baseURL = `http://localhost:4000/api`;
 // export const baseURL = `http://172.20.10.3:4000/api`;
 // test 
-export const baseURL = `http://192.168.8.151:4000/api`;
+export const baseURL = `http://192.168.8.18:4000/api`;
   // process.env.REACT_APP_API_URL || `http://localhost:4000/api`;
 
 //headers: defaultHeaders
